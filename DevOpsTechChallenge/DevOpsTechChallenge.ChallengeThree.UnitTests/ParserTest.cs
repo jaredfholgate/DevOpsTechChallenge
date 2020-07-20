2b61788233191e959a1d8290d2477d13329e6b9f
Jared Holgate <jared.holgate@maples.com>
2020-07-20 22:23:18 +0100

    Test passing to parse object

--- DevOpsTechChallenge/DevOpsTechChallenge.ChallengeThree.UnitTests/ParserTest.cs
+++ DevOpsTechChallenge/DevOpsTechChallenge.ChallengeThree.UnitTests/ParserTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace DevOpsTechChallenge.ChallengeThree.UnitTests
 {
@@ -11,6 +12,39 @@ namespace DevOpsTechChallenge.ChallengeThree.UnitTests
             var parser = new Parser();
             var result = parser.Parse("a/b/c", "{ \"a\" : { \"b\" : { \"c\" : \"d\" } } }");
             Assert.AreEqual("d", result);
+        }
+
+        [TestMethod]
+        public void CanAcceptADifferent3LevelKeyAndObjectAndReturnAValue()
+        {
+            var parser = new Parser();
+            var result = parser.Parse("x/y/z", "{ \"x\" : { \"y\" : { \"z\" : \"a\" } } }");
+            Assert.AreEqual("a", result);
+        }
+
+        [TestMethod]
+        public void CanAcceptA4LevelKeyAndObjectAndReturnAValue()
+        {
+            var parser = new Parser();
+            var result = parser.Parse("c/d/e/f", "{ \"c\" : { \"d\" : { \"e\" : {\"f\" : \"g\" } } } }");
+            Assert.AreEqual("g", result);
+        }
+
+        [TestMethod]
+        public void CanHandleMissingKey()
+        {
+            var parser = new Parser();
+            Assert.ThrowsException<ArgumentException>(() => parser.Parse(null, "{ \"c\" : { \"d\" : { \"e\" : {\"f\" : \"g\" } } } }"), "Key must contain a value.");
+            Assert.ThrowsException<ArgumentException>(() => parser.Parse(string.Empty, "{ \"c\" : { \"d\" : { \"e\" : {\"f\" : \"g\" } } } }"), "Key must contain a value.");
+        }
+
+
+        [TestMethod]
+        public void CanHandleMissingObject()
+        {
+            var parser = new Parser();
+            Assert.ThrowsException<ArgumentException>(() => parser.Parse("c/d/e/f", null), "Json must contain a value.");
+            Assert.ThrowsException<ArgumentException>(() => parser.Parse("c/d/e/f", string.Empty), "Json must contain a value.");
         }
     }
 }
--- DevOpsTechChallenge/DevOpsTechChallenge.ChallengeThree/Parser.cs
+++ DevOpsTechChallenge/DevOpsTechChallenge.ChallengeThree/Parser.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Data;
 
 namespace DevOpsTechChallenge.ChallengeThree
 {
@@ -6,7 +9,16 @@ namespace DevOpsTechChallenge.ChallengeThree
     {
         public string Parse(string key, string json)
         {
-            return "d";
+            if(string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentException("Key must contain a value.");
+            }
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                throw new ArgumentException("Json must contain a value.");
+            }
+            var parsedJson = JObject.Parse(json);
+            return parsedJson.SelectToken(key.Replace("/",".")).Value<string>();
         }
     }
 }
