d6c196deefd6faf3922b503d8c272704b182d021
Jared Holgate <jared.holgate@maples.com>
2020-07-21 13:26:29 +0100

    Fix up terraform

--- Infrastructure/dotc.tf
+++ Infrastructure/dotc.tf
@@ -16,13 +16,13 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "dotc" {
   name     = "__dotc_resourcegroupname__"
-  location = "West Europe"
+  location = "__azurelocation__"
 }
 
 resource "azurerm_sql_server" "dotc" {
   name                         = "__sqlservername__"
-  resource_group_name          = "${azurerm_resource_group.dotc.location}"
-  location                     = "${azurerm_resource_group.dotc.location}"
+  resource_group_name          = azurerm_resource_group.dotc.location
+  location                     = azurerm_resource_group.dotc.location
   version                      = "12.0"
   administrator_login          = "__sqladminusername__"
   administrator_login_password = "__sqladminpassword__"
@@ -30,15 +30,15 @@ resource "azurerm_sql_server" "dotc" {
 
 resource "azurerm_sql_database" "dotc" {
   name                = "__databasename__"
-  resource_group_name = "${azurerm_resource_group.dotc.location}"
-  location            = "${azurerm_resource_group.dotc.location}"
-  server_name         = "${azurerm_sql_server.dotc.name}"
+  resource_group_name = azurerm_resource_group.dotc.location
+  location            = azurerm_resource_group.dotc.location
+  server_name         = azurerm_sql_server.dotc.name
 }
 
 resource "azurerm_app_service_plan" "dotc" {
   name                = "__appserviceplan__"
-  location            = "${azurerm_resource_group.dotc.location}"
-  resource_group_name = "${azurerm_resource_group.dotc.name}"
+  location            = azurerm_resource_group.dotc.location
+  resource_group_name = azurerm_resource_group.dotc.name
 
   sku {
     tier = "Free"
@@ -48,21 +48,21 @@ resource "azurerm_app_service_plan" "dotc" {
 
 resource "azurerm_app_service" "dotc_api" {
   name                = "__appservicenameapi__"
-  location            = "${azurerm_resource_group.dotc.location}"
-  resource_group_name = "${azurerm_resource_group.dotc.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.dotc.id}"
+  location            = azurerm_resource_group.dotc.location
+  resource_group_name = azurerm_resource_group.dotc.name
+  app_service_plan_id = azurerm_app_service_plan.dotc.id
   connection_string {
     name  = "dotcContext"
     type  = "SQLServer"
-    value = "Server=${azurerm_sql_server.aadg.fully_qualified_domain_name} Integrated Security=SSPI"
+    value = "Server=tcp:${azurerm_sql_server.dotc.fully_qualified_domain_name},1433;Database=${azurerm_sql_database.dotc.name};User ID=${azurerm_sql_server.dotc.administrator_login};Password=${azurerm_sql_server.dotc.administrator_login_password}"
   }
 }
 
 resource "azurerm_app_service" "dotc_web" {
   name                = "__appservicenameweb__"
-  location            = "${azurerm_resource_group.dotc.location}"
-  resource_group_name = "${azurerm_resource_group.dotc.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.dotc.id}"
+  location            = azurerm_resource_group.dotc.location
+  resource_group_name = azurerm_resource_group.dotc.name
+  app_service_plan_id = azurerm_app_service_plan.dotc.id
   app_settings = {
     "APIUrl" = "${azurerm_app_service.dotc_api.default_site_hostname}"
   }
